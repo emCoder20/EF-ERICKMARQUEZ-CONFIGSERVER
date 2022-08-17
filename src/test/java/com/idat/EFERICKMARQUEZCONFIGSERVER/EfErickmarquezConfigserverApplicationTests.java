51d45527c6b656d0066e66310c2bb69a4b12189d
emCoder20 <100981739+emCoder20@users.noreply.github.com>
2022-08-17 00:48:02 -0500

    ef-erickmarquez

---
+++ src/test/java/com/idat/EFERICKMARQUEZCONFIGSERVER/EfErickmarquezConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFERICKMARQUEZCONFIGSERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfErickmarquezConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
